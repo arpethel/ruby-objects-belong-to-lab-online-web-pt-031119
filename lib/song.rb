0e7259bd7bfbcf099c74722749e1a9b72f6a200d
Ryan Pethel <arpethel@gmail.com>
2019-04-09 08:33:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,3 @@
 class Song
   attr_accessor :title, :artist
 end
-
-class Artist
-  attr_accessor :name, :genre
-end
-
-class Author
-  attr_accessor :name
-end
-
-class Post
-  attr_accessor :title, :author
-end
