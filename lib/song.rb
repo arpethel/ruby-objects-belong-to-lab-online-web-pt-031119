519ad5ace90c8c1302602ad94f1a8a7172dbd5b8
Ryan Pethel <arpethel@gmail.com>
2019-04-09 07:39:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :title, :artist
+end
+
+class Artist
+  attr_accessor :name, :genre
+end
+
+class Author
+  attr_accessor :name
+end
+
+class Post
+  attr_accessor :title, :author
+end
